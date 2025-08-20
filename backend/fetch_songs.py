ffbaa985426c13010cf0a77a8c1843e39ac5d80d
akelwalk <104277293+akelwalk@users.noreply.github.com>
2025-08-20 15:51:08 -0700

    pulled ~5000 songs into mongodb

--- backend/fetch_songs.py
+++ backend/fetch_songs.py
@@ -9,7 +9,6 @@ import random
 import requests
 import time
 import genres
-from collections import defaultdict
 
 
 load_dotenv()  # load variables from .env
@@ -37,82 +36,84 @@ db = client["musicdb"]
 tracks_collection = db["tracks"]
 
 # const variables
-NUM_SONGS_REQUESTED_PER_PLAYLIST = 3  # we are currently only getting 1 playlist per genre, and this many songs per playlist (max # of songs: 100)
-VALID_SONGS = 5
-# global variables
-db_count = 0
+NUM_PLAYLISTS_REQUESTED = 30
+NUM_SONGS_TAKEN_PER_PLAYLIST = 40  # we're pulling 100 (the max) songs per playlist, and randomly choosing a sample size of 40 from it
+MAX_NUM_SONGS_PER_PLAYLIST = 10 # out of the 40 randomly sampled songs from a playlist we're choosing 10 unique songs that have audio features 
+VALID_SONGS = 50 # we want to try getting 50 songs per genre
+RETRY_LIMIT = 30
 
+#TODO: didnt realize that some of the songs would conflict when writing to the db - ig for a final cleanup, i can take genres with less than 50 songs and then take one of the playlist at random and keep adding songs until the valid songs limit is reached
 
 # track the counts
 def fetch_tracks_per_genre():
-    global db_count
+    all_track_ids = set() # ensures that there arent duplicate track ids between genres
     for genre in genres.GENRES:
         track_map = {}
-        db_count = get_docs_from_mongo(track_map, genre)
+        prev_ids = set()  # keeps track of playlists we've already tried to extract songs from - i think its okay to have the same playlist for different genres
+        db_count = get_docs_from_mongo(track_map, all_track_ids, prev_ids, genre)
         if db_count >= VALID_SONGS:
-            print(f"Already have {db_count} {genre} songs in database")
+            print(f"Already have {db_count} {genre} songs in database.")
             continue
         
         playlists = get_playlists(genre)
-        prev_ids = set()  # keeps track of playlists we've already tried to extract songs from
-        retry_count = len(playlists)
-
-        while len(track_map) < (VALID_SONGS - db_count) and retry_count > 0:
-            chosen_pl = choose_playlist(playlists, prev_ids)  # randomly choose one playlist
+        i = 0
+        while len(track_map) < VALID_SONGS and i < RETRY_LIMIT:
+            print(
+            "#################################################################################################################################################################"
+            )
+            chosen_pl = choose_playlist(playlists, i) # randomly choose one playlist
             playlist_id = chosen_pl["id"]
-            tracks = get_playlist_tracks(playlist_id)  # get tracks for that playlist
+            # print(f"chosen playlist: {chosen_pl['name']} id: {playlist_id}")
+            tracks = get_playlist_tracks(playlist_id)  # gets (num songs taken per playlist) tracks for that playlist
             audio_features = get_audio_features(tracks)
             create_track_map(
-                track_map, tracks, audio_features, playlist_id
+                track_map, all_track_ids, tracks, audio_features, playlist_id
             )  # modifies the track_map
 
-            retry_count -= 1
+            i += 1 
             prev_ids.add(playlist_id)
 
         # store to mongodb
         count = store_tracks_in_mongo(track_map, genre)
+        
         print(
-            "##################################################################################################################################################################"
+            f"Retried {i} times out of {len(playlists)} possible retries."
         )
-        print(
-            f"Retried {len(playlists) - retry_count} times out of {len(playlists)} possible retries"
-        )
-        print(f"Number of new tracks stored for {genre}: {count}")
-        print(f"Number of {genre} already in database: {db_count}")
+        print(f"Number of {genre} already in database: {db_count}.")
+        print(f"Number of new tracks stored for {genre}: {count}.")
+        
 
 
 def remove_nones(list):
     return [p for p in list if p]
 
+def choose_playlist(playlists, i):
+    i = i % len(playlists)
+    return playlists[i]
 
 def get_playlists(genre):
     result = spotify_retry_request(
-        sp.search, q=genre, limit=10, offset=0, type="playlist"
+        sp.search, q=genre, limit=NUM_PLAYLISTS_REQUESTED, offset=0, type="playlist"
     )  # in case of rate limiting
     playlists = result["playlists"]["items"]  # getting the playlists
     playlists = remove_nones(playlists)  # remove none values
+    random.shuffle(playlists)
     return playlists
-
-
-# randomly chooses a single playlist given a list of playlists
-def choose_playlist(playlists, prev_ids):
-    index = random.randint(0, len(playlists) - 1)
-    chosen = playlists[index]
-    while (chosen["id"] in prev_ids):  # makes sure chosen playlist is different from previous ones
-        index = random.randint(0, len(playlists) - 1)
-        chosen = playlists[index]
-    return chosen
 
 
 # gets n tracks for a specific playlist id
 def get_playlist_tracks(playlist_id):
-    tracks = spotify_retry_request(
+    result = spotify_retry_request(
         sp.playlist_items,
         playlist_id=playlist_id,
         additional_types=["track"],
-        limit=NUM_SONGS_REQUESTED_PER_PLAYLIST,
+        limit=100, #requesting the first 100 tracks previously NUM_SONGS_TAKEN_PER_PLAYLIST
     )
-    return tracks["items"]
+    tracks = result["items"]
+    cleaned_tracks = [track for track in tracks if track and track.get("track") and track["track"].get("id")]
+
+    sampled_tracks = random.sample(cleaned_tracks, min(NUM_SONGS_TAKEN_PER_PLAYLIST, len(cleaned_tracks))) # returning a random sample of utilizing the first 100 tracks 
+    return sampled_tracks
 
 
 # gets ids of tracks
@@ -132,34 +133,33 @@ def get_audio_features(tracks):
     features = []
     url = "https://api.reccobeats.com/v1/audio-features"
     headers = {"Accept": "application/json"}
-    for id in track_ids:
-        if id:
-            params = {"ids": id}
-            response = recco_retry_request(
-                url=url, headers=headers, params=params
-            )  # in case of rate limiting
-            features.append(response.json()["content"])
+    t_ids = ",".join(track_ids)
+    params = {"ids": t_ids}
+    response = recco_retry_request(url=url, headers=headers, params=params)
+    features = response.json()["content"]
     return features
 
-
-def create_track_map(track_map, tracks, audio_features, playlist_id):
-    global db_count
+def create_track_map(track_map, all_track_ids, tracks, audio_features, playlist_id):
+    new_songs = 0
     for track, features in zip(tracks, audio_features):
         if not track or not track.get("track") or not features:
             continue
-        if len(track_map) >= (
-            VALID_SONGS - db_count
-        ):  # once we have reached the valid songs to add we can break out of this function, we don't need to add anything else to track_map
+
+        if new_songs >= MAX_NUM_SONGS_PER_PLAYLIST or len(track_map) >= VALID_SONGS:  # once we have reached the valid songs to add we can break out of this function, we don't need to add anything else to track_map
             break
 
         t_id = track["track"]["id"]
-        if t_id not in track_map:
+        if t_id not in all_track_ids: # track_map only keeps track of tids for current genre
             track_map[t_id] = [track["track"], features, playlist_id]
+            all_track_ids.add(t_id)
+            new_songs += 1
 
 
 def store_tracks_in_mongo(track_map, genre):
     count = 0
     for t_id, data in track_map.items():  # this only contains valid tracks and features
+        # if count >= REAL_LIMIT:
+        #     break
         track = data[0]  # this is already track["track"]
         features = data[1]
         playlist_id = data[2]
@@ -176,7 +176,7 @@ def store_tracks_in_mongo(track_map, genre):
                 "images": track["album"].get("images", []),
                 "track_link": track["external_urls"]["spotify"],
             },
-            "audio_features": features[0],
+            "audio_features": features,
             "playlist_id": playlist_id,
             "genre": genre,
             "emb": {"triplet64": None, "ae32": None},  # leaving blank for now
@@ -192,11 +192,13 @@ def store_tracks_in_mongo(track_map, genre):
     return count
 
 
-def get_docs_from_mongo(track_map, genre):
+def get_docs_from_mongo(track_map, all_track_ids, prev_ids, genre):
     cursor = tracks_collection.find({"genre": genre})
     count = tracks_collection.count_documents({"genre": genre})
     
     for doc in cursor:
+        all_track_ids.add(doc["_id"])
+        prev_ids.add(doc["playlist_id"])
         track_map[doc["_id"]] = ["", "", ""] #info stored is irrelevant because we're gonna skip over these tracks, they're already in the database
     return count
 
@@ -208,8 +210,8 @@ def spotify_retry_request(func, *args, **kwargs):
         except SpotifyException as e:
             if e.http_status == 429:
                 retry_after = int(
-                    e.headers.get("Retry-After", 5)
-                )  # default to 5 sec if retry-after header is missing
+                    e.headers.get("Retry-After", 180)
+                )  # default to 180 sec if retry-after header is missing
                 print(f"Rate limited by Spotify. Waiting {retry_after} seconds...")
                 time.sleep(retry_after)
             else:
@@ -221,7 +223,7 @@ def recco_retry_request(url, headers, params):
     while True:
         response = requests.request("GET", url=url, headers=headers, params=params)
         if response.status_code == 429:
-            retry_after = int(response.headers.get("Retry-After", 5))
+            retry_after = int(response.headers.get("Retry-After", 180))
             print(f"Rate limited by Reccobeats. Waiting {retry_after} seconds...")
             time.sleep(retry_after)
         else:

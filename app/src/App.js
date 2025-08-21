a85d722207b03807404144126b3408cf306d0e6c
aspsnakes <77006857+aspsnakes@users.noreply.github.com>
2025-08-20 23:56:06 -0700

    basic frontend and landing page setup

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <section className="main">
+      <div className="main-right" />
+
+      <div className="main-content">
+        <h1>MusicMatch</h1>
+        <button className="btn">Login</button>
+      </div>
+    </section>
+  );
+}
